994c848707e6bbe07e584154d9331e03fb1e93cf
BitBit69 <64637014+BitBit69@users.noreply.github.com>
2021-08-13 21:47:23 -0400

    Update Xperimental_Movement.cs
    actually fixed it

--- Xperimental_Movement.cs
+++ Xperimental_Movement.cs
@@ -2,37 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public float fwdSpeed = 1000;
-public float bckSpeed = 1000;
-public float rotSpeed = 1000;
-public float boostMult = 1.5f;
-public Rigidbody cubebody;
-public class Xperimental_Movement_Control : MonoBehaviour
+public class XperimentalMovement : MonoBehaviour
 {
+	public float fwdSpeed = 1000;
+	public float bckSpeed = 1000;
+	public float rotSpeed = 1000;
+	public float boostMult = 1.5f;
+	public Rigidbody cubebody;
 	void Start()
 	{
 		cubebody = GetComponent<Rigidbody>();
 	}
 	void Update()
 	{
-		if(Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.W))
 		{
-			transform.Position(Vector3.forward * fwdSpeed * (Input.GetAxis("boost") * boostMult) * Time.fixedDeltaTime);
+			transform.Translate(Vector3.forward * fwdSpeed * (Input.GetAxis("boost") * boostMult) * Time.fixedDeltaTime);
 		}
-		else if(Input.GetKey(KeyCode.S))
+		else if (Input.GetKey(KeyCode.S))
 		{
-			transform.Position(Vector3.back * bckSpeed * (Input.GetAxis("boost") * boostMult) * Time.fixedDeltaTime);
+			transform.Translate(Vector3.back * bckSpeed * (Input.GetAxis("boost") * boostMult) * Time.fixedDeltaTime);
 		}
 		else;
 		{
 			Debug.Log("no key is pressed!");
 		}
-		if(Input.GetKey(KeyCode.A))
+		if (Input.GetKey(KeyCode.A))
 		{
 			transform.Rotate(Vector3.left * rotSpeed * Time.fixedDeltaTime);
 		}
-		else if(Input.GetKey(KeyCode.D))
+		else if (Input.GetKey(KeyCode.D))
 		{
 			transform.Rotate(Vector3.right * rotSpeed * Time.fixedDeltaTime);
 		}
